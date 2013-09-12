b38f3c9aef7828a5414d4c4c0edf7c6b8e89ce3c
Gabriela Limonta <glimonta@gmail.com>
2013-09-12 10:38:31 -0430

    Terminada clase posee. XOXO

--- Clases/Posee.java
+++ Clases/Posee.java
@@ -1,8 +1,11 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 
@@ -51,8 +54,8 @@ public class Posee {
             st = conn.createStatement();
             
             st.execute("insert into posee values ('"+ this.producto.codigoProd +
-                    "', '" + this.servicio.nombre + "', '" + this.fecha.toString() + 
-                    "', '" + this.cantidad + "');");
+                    "', '" + this.adicional.nombre + "', '" + this.fechaInicio.toString() + 
+                     "');");
             
             
             conn.close();
@@ -61,6 +64,64 @@ public class Posee {
             System.err.println(ex.getMessage());
         }
     }
-    
+     
+    static Posee consultarPosee(Integer id, String nombre_servicio, java.sql.Date fecha) throws ParseException {
+        Posee posee = new Posee();
+        
+        try {
+            String url = "jdbc:postgresql:innova";
+            Properties props = new Properties();
+            props.setProperty("user","gabriela");
+            props.setProperty("password","wennicheinjungewar");
+            Connection conn = DriverManager.getConnection(url,props);
+            
+            Statement st;
+            st = conn.createStatement();
+             
+            ResultSet rs = st.executeQuery("select * "
+                    + "from posee where id = '" + id 
+                    + "' and nombre_servicio = '" + nombre_servicio + "' and"
+                    + " fecha_inic= '" + fecha.toString()
+                    + "';");
+            
+            rs.next();
+              
+            java.util.Date utilDate = new SimpleDateFormat("yyyy-MM-dd").parse(rs.getString(2));
+            fecha = new java.sql.Date(utilDate.getTime());
+            
+            
+            Producto producto = Producto.consultarProducto(id);
+            ServicioAdicional servicioAdicional = ServicioAdicional.consultarServicioAdicional(nombre_servicio);
+            
+            posee = new Posee(rs.getInt(1), fecha, servicioAdicional, producto);
+            
+            
+          conn.close();
+            
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        
+        return posee;
+    }
+     
+    void eliminarPosee() {
+        String url = "jdbc:postgresql:innova";
+        Properties props = new Properties();
+        props.setProperty("user","gabriela");
+        props.setProperty("password","wennicheinjungewar");
+       
+        try (Connection conn = DriverManager.getConnection(url,props)) {
+            Statement st;
+            st = conn.createStatement();
+                        
+            st.execute("delete from posee where id =' " + 
+                    this.producto.codigoProd + "' and nombre_servicio ='" + this.adicional.nombre + 
+                    "' and fecha_inic = '" + this.fechaInicio + "';");
+        
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
     
 }
