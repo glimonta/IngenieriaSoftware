9f2498bbba2044300a76e3566cd715aef93ad62b
John Delgado <johndelgado@MacBook-Pro-de-John.local>
2013-09-14 15:06:37 -0430

    Refinados imports

--- Clases/gestionarConsumo.java
+++ Clases/gestionarConsumo.java
@@ -1,8 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
